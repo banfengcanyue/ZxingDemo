a21cbd176b7a9a8dd5c1224976eaf224e7b5b576
Wu <banfengcanyue@163.com>
2020-06-22 16:48:45 +0800

    第一次提交

---
+++ app/src/main/java/com/bfcy/zxingdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.bfcy.zxingdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.zxing.client.android.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
