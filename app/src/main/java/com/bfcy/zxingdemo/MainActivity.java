34bf59c3c1f77e5d5aeb979f516c191261fce6a7
banfengcanyue <banfengcanyue@163.com>
2020-10-12 10:23:51 +0800

    添加权限申请，插件和基础库更新

--- app/src/main/java/com/bfcy/zxingdemo/MainActivity.java
+++ app/src/main/java/com/bfcy/zxingdemo/MainActivity.java
@@ -1,10 +1,16 @@
 package com.bfcy.zxingdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.zxing.client.android.CaptureActivity;
 
@@ -17,9 +23,39 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
-                startActivity(intent);
+                requestPermission();
             }
         });
+    }
+
+    private void requestPermission() {
+        int hasPermission = ContextCompat.checkSelfPermission(getApplication(), Manifest.permission.CAMERA);
+        if (hasPermission == PackageManager.PERMISSION_GRANTED) {
+            //已获取权限
+            goScan();
+        } else {
+            //未获取权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        }
+    }
+
+    private void goScan() {
+        Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //用户同意了权限申请
+                goScan();
+            } else {
+                //用户拒绝了权限申请，建议向用户解释权限用途
+                Toast.makeText(MainActivity.this, "扫描功能需要相机权限", Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 }
